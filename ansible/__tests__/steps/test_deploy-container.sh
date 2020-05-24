357aa1a3be97b19f55d823ec8c21eb01681fb8c5
Mario Lubenka <mario.lubenka@googlemail.com>
2020-05-24 14:17:51 +0200

    tests: move TEST variable to command

--- ansible/__tests__/steps/test_deploy-container.sh
+++ ansible/__tests__/steps/test_deploy-container.sh
@@ -2,19 +2,16 @@
 # IP address in environment "IP"
 # Domain in environment "DOMAIN"
 INVENTORY_PATH=ansible/__tests__/inventory.yml
-TEST=1
 
 # This test deploys a container project onto the target server
 # IMPORTANT: This must run after test_provision.sh!
 
-
 sed -e "s/\${ipaddress}/${IP}/" -e "s/\${application}/container/" ansible/__tests__/inventory.dist.yml > ansible/__tests__/inventory.yml
 sed -e "s/\${domain}/${DOMAIN}/" ansible/__tests__/projects/container.dist.yml > ansible/__tests__/projects/container.yml
-ansible-playbook ansible/application-deploy.yml -i $INVENTORY_PATH
-wget -O fakelerootx1.pem https://letsencrypt.org/certs/fakelerootx1.pem
-content=$(wget ${DOMAIN} -ca-certificate=./fakelerootx1.pem --https-only -q -O -)
+TEST=1 ansible-playbook ansible/application-deploy.yml -i $INVENTORY_PATH -vv
+content=$(wget --no-check-certificate --https-only -q -O - ${DOMAIN})
 if [[ $content != *"Hello world!"* ]]; then
   echo "HTTP content check on container project failed" 1>&2
   exit 1
 fi
-ansible-playbook ansible/application-destroy.yml -i $INVENTORY_PATH --extra-vars "project_name=container"
+TEST=1 ansible-playbook ansible/application-destroy.yml -i $INVENTORY_PATH --extra-vars "project_name=container" -vv
