2b02ab32aea6ab010c9884094f455e27e98db098
Mario Lubenka <mario.lubenka@googlemail.com>
2020-06-07 19:31:14 +0200

    docs: set workflow as default page

--- Documentation/.build/src/pages/index.js
+++ Documentation/.build/src/pages/index.js
@@ -4,7 +4,7 @@ import {Redirect} from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function Home() {
-  return <Redirect to={useBaseUrl('/introduction/getting-started')} />;
+  return <Redirect to={useBaseUrl('/introduction/workflow')} />;
 }
 
 export default Home;
