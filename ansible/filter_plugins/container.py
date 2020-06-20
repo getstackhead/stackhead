5c38100a69eedd8d210304560addfc4ff383c640
Mario Lubenka <mario.lubenka@googlemail.com>
2020-06-20 14:08:03 +0200

    feat: introduce better port-index mapping

--- ansible/filter_plugins/container.py
+++ ansible/filter_plugins/container.py
@@ -5,10 +5,10 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'containerPortsStr': self.container_ports_str
+            'containerPorts': self.container_ports
         }
 
-    def container_ports_str(self, containerapp__expose, project_name, split_char):
+    def container_ports(self, containerapp__expose, project_name):
         containerapp__expose.sort(key=lambda x: x['service'])
         output = []
 
@@ -17,14 +17,19 @@ class FilterModule(object):
         index = 0
         for nginx_expose in containerapp__expose:
             service_name = nginx_expose['service']
-            identifier = service_name + '-' + str(nginx_expose['internal_port'])
+            internal_port = nginx_expose['internal_port']
+            identifier = service_name + '-' + str(internal_port)
             if service_name is None or nginx_expose['external_port'] == 443 or identifier in processed:
                 continue
             if previous_service != service_name:
                 index = 0
-            output.append("${docker_container.stackhead-" + project_name + "-" + service_name + ".ports[" + str(index) + "].external}")
+            output.append({
+                'service': service_name,
+                'internal_port': internal_port,
+                'tfstring': "${docker_container.stackhead-" + project_name + "-" + service_name + ".ports[" + str(index) + "].external}"
+            })
             previous_service = service_name
             index += 1
             processed.append(identifier)
 
-        return split_char.join(output)
+        return output
