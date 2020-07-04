4d9a5200dd7e6ee4128b76966fbef4c9bd71663c
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-04 17:12:05 +0200

    feat: add PyOpenSSL pip requirement

--- ansible/__tests__/steps/test_provision.sh
+++ ansible/__tests__/steps/test_provision.sh
@@ -7,6 +7,7 @@ export TEST=1
 sed -e "s/\${ipaddress}/${IP}/" ansible/__tests__/inventory.dist.yml > $INVENTORY_PATH
 
 # Install dependencies
+pip install -r ansible/requirements/pip.txt
 ansible-galaxy install -r ansible/requirements/requirements.yml
 
 # Provision server
