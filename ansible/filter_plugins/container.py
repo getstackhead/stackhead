e5012315d9f8df316d65cfb483b8f6ed53d3ed23
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-24 16:30:05 +0200

    feat: replace (some)) Terraform calls with Puppet

--- ansible/filter_plugins/container.py
+++ ansible/filter_plugins/container.py
@@ -1,3 +1,5 @@
+import re
+
 class FilterModule(object):
     """
     Jinja2 filters for container related stuff
@@ -5,8 +7,26 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'containerPorts': self.container_ports
+            'containerPorts': self.container_ports,
+            'containerVolumes': self.container_volumes
         }
+
+    def container_volumes(self, service, containerapp__project_name):
+        if 'volumes' not in service:
+            return []
+        volumes = service['volumes']
+        volume_paths = []
+        for volume in volumes:
+            if volume['type'] == "custom":
+                volume_paths.append(volume['src'] + ':' + volume['dest'])
+            else:
+                sanitize = re.compile(r'[^\w]')
+                suffix = sanitize.sub('_', volume['src'])
+                if volume['type'] == "local":
+                    suffix = service['name'] + "-" + suffix
+                src = volume['type'] + "-" + containerapp__project_name + "-" + suffix
+                volume_paths.append(src + ':' + volume['dest'])
+        return volume_paths
 
     def container_ports(self, containerapp__expose, project_name):
         containerapp__expose.sort(key=lambda x: x['service'])
