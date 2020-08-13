d3a08f277585c20f884731918b795da1381b86e4
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-13 17:57:38 +0200

    test(provision): remove pip install command

--- ansible/__tests__/steps/test_provision.sh
+++ ansible/__tests__/steps/test_provision.sh
@@ -6,7 +6,6 @@ INVENTORY_PATH=ansible/__tests__/inventory.yml
 sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" ansible/__tests__/inventory.dist.yml > $INVENTORY_PATH
 
 # Install dependencies
-pip install -r ansible/requirements/pip.txt
 ansible-galaxy install -r ansible/requirements/requirements.yml
 
 # Provision server
