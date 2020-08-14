def35e20235daee2cf9a737cf0f1f926fab27b4a
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-14 18:59:53 +0200

    tests: simplify http check

--- ansible/__tests__/steps/test_deploy-container.sh
+++ ansible/__tests__/steps/test_deploy-container.sh
@@ -6,30 +6,30 @@ INVENTORY_PATH=ansible/__tests__/inventory.yml
 # This test deploys a container project onto the target server
 # IMPORTANT: This must run after test_provision.sh!
 
+function http_check() {
+  echo "Checking HTTP content on ${1}"
+  if [[ "${3}" != "" && "${4}" != "" ]]; then
+    CONTENT=$(wget --no-check-certificate --https-only --http-user="${3}" --http-password="${4}" -q -O - "${1}")
+  else
+    CONTENT=$(wget --no-check-certificate --https-only -q -O - "${1}")
+  fi
+  if [[ $CONTENT != *"${2}"* ]]; then
+    echo "HTTP content check failed: ${CONTENT}" 1>&2
+    exit 1
+  else
+    echo "HTTP content check succeeded"
+  fi
+}
+
 sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" -e "s/\${application}/container/" ansible/__tests__/inventory.dist.yml > ansible/__tests__/inventory.yml
 sed -e "s/\${domain}/${DOMAIN}/" ansible/__tests__/projects/container.dist.yml > ansible/__tests__/projects/container.yml
 TEST=1 ansible-playbook ansible/application-deploy.yml -i $INVENTORY_PATH -vv
-URL="https://${DOMAIN}"
-CONTENT=$(wget --no-check-certificate --https-only -q -O - "${URL}")
-echo "Checking HTTP content on ${URL}"
-if [[ $CONTENT != *"Hello world!"* ]]; then
-  echo "HTTP content check on container project failed: ${CONTENT}" 1>&2
-  exit 1
-fi
-# test that phpmyadmin is available
-URL="https://${DOMAIN}:81"
-CONTENT=$(wget --no-check-certificate --https-only -q -O - "${URL}")
-echo "Checking HTTP content on ${URL}"
-if [[ $CONTENT != *"phpMyAdmin"* ]]; then
-  echo "HTTP content check on phpmyadmin in container project failed: ${CONTENT}" 1>&2
-  exit 1
-fi
-# test that phpmyadmin is available on subdomain
-URL="https://sub.${DOMAIN}"
-CONTENT=$(wget --no-check-certificate --http-user=user --http-password=pass --https-only -q -O - "${URL}")
-echo "Checking HTTP content on ${URL}"
-if [[ $CONTENT != *"phpMyAdmin"* ]]; then
-  echo "HTTP content check on subdomain phpmyadmin in container project failed: ${CONTENT}" 1>&2
-  exit 1
-fi
-#TEST=1 ansible-playbook ansible/application-destroy.yml -i $INVENTORY_PATH --extra-vars "project_name=container" -vv
+
+# wait some time for server to finish
+sleep 5s
+
+http_check "https://${DOMAIN}" "Hello world!"
+http_check "https://${DOMAIN}:81" "phpMyAdmin"
+http_check "https://sub.${DOMAIN}" "phpMyAdmin" "user" "pass"
+
+TEST=1 ansible-playbook ansible/application-destroy.yml -i $INVENTORY_PATH --extra-vars "project_name=container" -vv
