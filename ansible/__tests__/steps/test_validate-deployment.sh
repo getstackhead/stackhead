8576388dabdda8359631d51441f367dd3d46ba78
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-15 14:15:55 +0200

    test: add ssl check

--- ansible/__tests__/steps/test_validate-deployment.sh
+++ ansible/__tests__/steps/test_validate-deployment.sh
@@ -2,12 +2,17 @@
 # Domain in environment "DOMAIN"
 # IMPORTANT: This must run after test_deploy.sh!
 
+function ssl_check() {
+  echo "Checking SSL certificate on ${1}"
+  openssl s_client -connect "${1}:443" -servername "${1}"
+}
+
 function http_check() {
   echo "Checking HTTP content on ${1}"
   if [[ "${3}" != "" && "${4}" != "" ]]; then
-    CONTENT=$(curl --insecure -v -u "${3}:${4}" "${1}")
+    CONTENT=$(curl --insecure -v -u "${3}:${4}" "https://${1}")
   else
-    CONTENT=$(curl --insecure -v "${1}")
+    CONTENT=$(curl --insecure -v "https://${1}")
   fi
   if [[ $CONTENT != *"${2}"* ]]; then
     echo "HTTP content check failed: ${CONTENT}" 1>&2
@@ -17,6 +22,13 @@ function http_check() {
   fi
 }
 
-http_check "https://${DOMAIN}" "Hello world!"
-http_check "https://${DOMAIN}:81" "phpMyAdmin"
-http_check "https://sub.${DOMAIN}" "phpMyAdmin" "user" "pass"
+openssl version
+curl -V
+ping -c 5 ${DOMAIN}
+ping -c 5 sub.${DOMAIN}
+
+ssl_check "${DOMAIN}"
+ssl_check "sub.${DOMAIN}"
+http_check "${DOMAIN}" "Hello world!"
+http_check "${DOMAIN}:81" "phpMyAdmin"
+http_check "sub.${DOMAIN}" "phpMyAdmin" "user" "pass"
