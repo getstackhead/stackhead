ad288810020ec5c304905c6f50b0bf9afcdf6e21
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-22 15:44:53 +0200

    test: install nginx module

--- ansible/__tests__/steps/test_provision.sh
+++ ansible/__tests__/steps/test_provision.sh
@@ -12,7 +12,10 @@ sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" ansible/__tes
 # Install dependencies
 STACKHEAD_COLLECTION_PATH=~/.ansible/collections/ansible_collections/getstackhead/stackhead
 pip install -r "$STACKHEAD_COLLECTION_PATH"/requirements/pip.txt
+
+# Install requirements
 ansible-galaxy install -r "$STACKHEAD_COLLECTION_PATH"/requirements/requirements.yml
+ansible-galaxy install getstackhead.stackhead_webserver_nginx
 
 # Provision server
 TEST=1 ansible-playbook  "$STACKHEAD_COLLECTION_PATH"/playbooks/server-provision.yml -i "${INVENTORY_PATH}" -vv
