54f752af0ae2181dd85cb3a6459d7f94a89541b9
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-27 22:21:18 +0200

    fix: set inventory path

--- tests/steps/test_deploy.sh
+++ tests/steps/test_deploy.sh
@@ -2,6 +2,7 @@
 # This test deploys a container project onto the target server
 # IMPORTANT: This must run after test_provision.sh!
 
-sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${application}/container/" ./tests/inventory.dist.yml > ./tests/inventory.yml
+INVENTORY_PATH=./tests/inventory.yml
+sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${application}/container/" ./tests/inventory.dist.yml > "${INVENTORY_PATH}"
 sed -e "s/\${domain}/${INPUT_DOMAIN}/" ./tests/projects/container.dist.yml > ./tests/projects/container.yml
 TEST=1 ansible-playbook ./ansible/application-deploy.yml -i "${INVENTORY_PATH}" -vv
