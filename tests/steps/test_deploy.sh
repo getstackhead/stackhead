2739105e057d107feb4c8b10b88a9dffb5e7d5e9
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-27 23:25:07 +0200

    test: use github action path

--- tests/steps/test_deploy.sh
+++ tests/steps/test_deploy.sh
@@ -2,8 +2,8 @@
 # This test deploys a container project onto the target server
 # IMPORTANT: This must run after test_provision.sh!
 
-INVENTORY_PATH=./tests/inventory.yml
-sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${application}/container/" ./tests/inventory.dist.yml > "${INVENTORY_PATH}"
-sed -e "s/\${domain}/${INPUT_DOMAIN}/" ./tests/projects/container.dist.yml > ./tests/projects/container.yml
+INVENTORY_PATH=${ACTION_PATH}/tests/inventory.yml
+sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${application}/container/" ${ACTION_PATH}/tests/inventory.dist.yml > "${INVENTORY_PATH}"
+sed -e "s/\${domain}/${INPUT_DOMAIN}/" ${ACTION_PATH}/tests/projects/container.dist.yml > ${ACTION_PATH}/tests/projects/container.yml
 
-TEST=1 ansible-playbook ./ansible/application-deploy.yml -i "${INVENTORY_PATH}" -vv
+TEST=1 ansible-playbook ${ACTION_PATH}/ansible/application-deploy.yml -i "${INVENTORY_PATH}" -vv
--- tests/steps/test_destroy.sh
+++ tests/steps/test_destroy.sh
@@ -6,5 +6,5 @@
 # This test destroys a deployed project on the target server
 # IMPORTANT: This must run after test_deploy.sh!
 
-INVENTORY_PATH=./tests/inventory.yml
-TEST=1 ansible-playbook ./ansible/application-destroy.yml -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
+INVENTORY_PATH=${ACTION_PATH}/tests/inventory.yml
+TEST=1 ansible-playbook ${ACTION_PATH}/ansible/application-destroy.yml -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
