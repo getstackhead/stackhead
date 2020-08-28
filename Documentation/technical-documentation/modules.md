a27bf0801d98cfeee468dfee287e21620487be70
Mario Lubenka <mario.lubenka@googlemail.com>
2020-08-28 18:06:31 +0200

    feat: GitHub action for integration tests (#42)
    * tests: add tests directory
    
    * ci: use ghaction for integration test
    
    * fix: set inventory path
    
    * fix: optional role name
    
    * debug
    
    * fix: domain
    
    * debug
    
    * fix: set env
    
    * test: use github action path
    
    * style: fix linter issues
    
    * style: fix linter issues
    
    * ci: remove duplicate test
    
    * chore: move test files
    
    * style: fix linter
    
    * docs: update documentation on GHa action
    
    * feat: make second domain configurable

--- Documentation/technical-documentation/modules.md
+++ Documentation/technical-documentation/modules.md
@@ -117,3 +117,26 @@ You'll find the files inside the project's certificate directory:
 The certificate files will be created after your role was executed. If you need to access the certificate files within your role, please execute Terraform as described above.
 {% endhint %}
 
+### Testing
+
+There is a GitHub action for integration testing.
+
+The action will:
+
+* deploy to a server (specified by `ipaddress` input)
+* setup a multi-container application and made it available at two domains (specified by `domain` and `domain2`)
+* test that content is served correctly
+* tear down the deployed application
+
+In order to use the action you'll have to set up a webserver and make sure GitHub can connect via SSH onto it.
+Set up two TLDs (or subdomains) and point their DNS A record to that IP address.
+
+```yaml
+- uses: getstackhead/stackhead@master
+  with:
+    ipaddress: 'your ip address'
+    domain: 'yourdomain.com'
+    domain2: 'yourdomain2.com'
+    webserver: 'getstackhead.stackhead_webserver_nginx' # webserver to use (make sure to install it)
+    rolename: '<vendor>.stackhead_<type>_<name>'
+```
--- ansible/__tests__/steps/test_provision.sh
+++ ansible/__tests__/steps/test_provision.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# IP address in environment "IP"
-# Domain in environment "DOMAIN"
-INVENTORY_PATH=ansible/__tests__/inventory.yml
+# IP address in environment "INPUT_IPADDRESS"
+INVENTORY_PATH=${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.yml
 
-# map webserver name to role name
-if [ "$WEBSERVER" == 'nginx' ]; then WEBSERVER='getstackhead.stackhead_webserver_nginx'; fi
-if [ "$WEBSERVER" == 'caddy' ]; then WEBSERVER='getstackhead.stackhead_webserver_caddy'; fi
-
-sed -e "s/\${ipaddress}/${IP}/" -e "s/\${webserver}/${WEBSERVER}/" ansible/__tests__/inventory.dist.yml > $INVENTORY_PATH
+sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" "${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.dist.yml" > "${INVENTORY_PATH}"
 
 # Install dependencies
-pip install -r ansible/requirements/pip.txt
-ansible-galaxy install -r ansible/requirements/requirements.yml
+ansible-galaxy install -r "${GITHUB_ACTION_PATH}/ansible/requirements/requirements.yml" --force-with-deps
+
+if [[ $INPUT_ROLENAME != '' ]]; then
+  # Remove this role and set symlink
+  rm -rf "${HOME}/.ansible/roles/${INPUT_ROLENAME}"
+  ln -s "${GITHUB_WORKSPACE}" "${HOME}/.ansible/roles/${INPUT_ROLENAME}"
+fi
 
 # Provision server
-TEST=1 ansible-playbook ansible/server-provision.yml -i "${INVENTORY_PATH}" -vv
+TEST=1 ansible-playbook "${GITHUB_ACTION_PATH}/ansible/server-provision.yml" -i "${INVENTORY_PATH}" -vv
