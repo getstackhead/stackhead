679a22dd903a0a1efbbfcb435a51cdf5ae294e1b
Mario Lubenka <mario.lubenka@googlemail.com>
2020-09-02 19:46:52 +0200

    tests(action): add selftest input variable

--- ansible/__tests__/steps/build.sh
+++ ansible/__tests__/steps/build.sh
@@ -3,7 +3,11 @@ ansible-galaxy collection build -f "${GITHUB_ACTION_PATH}/ansible"
 ansible-galaxy collection install "$(find getstackhead-stackhead-*)" -f
 
 # Install dependencies
-ansible-galaxy install -r "${GITHUB_ACTION_PATH}/ansible/requirements/requirements.yml" --force-with-deps
+if [[ $INPUT_SELFTEST != '' ]]; then
+  ansible-galaxy install -r "${GITHUB_WORKSPACE}/ansible/requirements/requirements.yml" --force-with-deps
+else
+  ansible-galaxy install -r "${GITHUB_ACTION_PATH}/ansible/requirements/requirements.yml" --force-with-deps
+fi
 
 if [[ $INPUT_ROLENAME != '' ]]; then
   # Remove this role and set symlink
