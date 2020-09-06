98529b641c648734465d27b6a7b0dc2d909b93b3
Mario Lubenka <mario.lubenka@googlemail.com>
2020-09-06 15:29:58 +0200

    ci: add GITHUB_TOKEN to linter workflow

--- ansible/__tests__/steps/build.sh
+++ ansible/__tests__/steps/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-ansible-galaxy collection build -f "${GITHUB_ACTION_PATH}/ansible"
-ansible-galaxy collection install "$(find getstackhead-stackhead-*)" -f
 
-# Install dependencies
-if [[ $INPUT_SELFTEST != '' ]]; then
-  ansible-galaxy install -r "${GITHUB_WORKSPACE}/ansible/requirements/requirements.yml" --force-with-deps
+if [[ $INPUT_CLI != '' ]]; then
+  # Write config file
+  echo "---
+modules:
+  webserver: ${INPUT_WEBSERVER}
+  container: ${INPUT_CONTAINER}
+" > "/tmp/.stackhead-cli.yml"
+
+  if [[ $INPUT_SELFTEST != '' ]]; then
+    ${INPUT_CLI_BIN_PATH} init --version="${INPUT_VERSION}" -c "/tmp/.stackhead-cli.yml"
+  else
+    ${INPUT_CLI_BIN_PATH} init --version=next -c "/tmp/.stackhead-cli.yml"
+  fi
 else
-  ansible-galaxy install -r "${GITHUB_ACTION_PATH}/ansible/requirements/requirements.yml" --force-with-deps
+  ansible-galaxy collection build -f "${GITHUB_ACTION_PATH}/ansible"
+  ansible-galaxy collection install "$(find getstackhead-stackhead-*)" -f
+
+  # Install dependencies
+  if [[ $INPUT_SELFTEST != '' ]]; then
+    ansible-galaxy install -r "${GITHUB_WORKSPACE}/ansible/requirements/requirements.yml" --force-with-deps
+  else
+    ansible-galaxy install -r "${GITHUB_ACTION_PATH}/ansible/requirements/requirements.yml" --force-with-deps
+  fi
 fi
 
 if [[ $INPUT_ROLENAME != '' ]]; then
--- ansible/__tests__/steps/destroy.sh
+++ ansible/__tests__/steps/destroy.sh
@@ -4,5 +4,9 @@
 # This test destroys a deployed project on the target server
 # IMPORTANT: This must run after test_deploy.sh!
 
-INVENTORY_PATH="${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.yml"
-TEST=1 ansible-playbook "${GITHUB_ACTION_PATH}/ansible/playbooks/application-destroy.yml" -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
+if [[ $INPUT_CLI != '' ]]; then
+  ${INPUT_CLI_BIN_PATH} destroy container "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml"
+else
+  INVENTORY_PATH="${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.yml"
+  TEST=1 ansible-playbook "${GITHUB_ACTION_PATH}/ansible/playbooks/application-destroy.yml" -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
+fi
