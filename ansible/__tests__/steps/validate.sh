24507df46a619397da3abc9fcf3cd4ef9f9fbdbf
Mario Lubenka <mario.lubenka@googlemail.com>
2020-09-19 16:15:59 +0200

    tests(steps/validate): remove SSL check

--- ansible/__tests__/steps/validate.sh
+++ ansible/__tests__/steps/validate.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # IMPORTANT: This must run after test_deploy.sh!
 
-function ssl_check() {
-	echo "Checking SSL certificate on ${1}"
-	openssl s_client -connect "${1}:443" -servername "${1}"
-}
-
 function http_check() {
 	echo "Checking HTTP content on ${1}"
 	if [[ "${3}" != "" && "${4}" != "" ]]; then
@@ -26,8 +21,6 @@ curl -V
 ping -c 5 "${INPUT_DOMAIN}"
 ping -c 5 "${INPUT_DOMAIN2}"
 
-ssl_check "${INPUT_DOMAIN}"
-ssl_check "${INPUT_DOMAIN2}"
 http_check "${INPUT_DOMAIN}" "Hello world!"
 http_check "${INPUT_DOMAIN}:81" "phpMyAdmin"
 http_check "${INPUT_DOMAIN2}" "phpMyAdmin" "user" "pass"
