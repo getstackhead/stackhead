4193dc20bb585a900ecbb4beda8e23d5511b5b9e
Mario Lubenka <mario.lubenka@googlemail.com>
2020-09-22 17:32:24 +0200

    fix(cli): show actual error message that caused abort

--- cli/routines/exec.go
+++ cli/routines/exec.go
@@ -15,6 +15,7 @@ import (
 // Exec is a wrapper function around exec.Command with additional settings for this CLI
 func Exec(name string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
+	var outBuffer = new(bytes.Buffer)
 	var errBuffer = new(bytes.Buffer)
 	if viper.GetBool("verbose") {
 		_, err := fmt.Fprintf(os.Stdout, "Executing command: %s\n", strings.Join(append([]string{name}, arg...), " "))
@@ -24,10 +25,19 @@ func Exec(name string, arg ...string) error {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stdout
 	} else {
+		cmd.Stdout = outBuffer
 		cmd.Stderr = errBuffer
 	}
 	if err := cmd.Run(); err != nil {
-		return fmt.Errorf(errBuffer.String())
+		lines := strings.Split(outBuffer.String(), "\n")
+		filtered := []string{errBuffer.String()}
+		for _, x := range lines {
+			if strings.HasPrefix(x, "fatal:") {
+				filtered = append(filtered, x)
+			}
+		}
+
+		return fmt.Errorf(strings.Join(filtered, "\n"))
 	}
 	return nil
 }
