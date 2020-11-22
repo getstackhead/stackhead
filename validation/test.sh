5229ee68abc430637116a05289a51422f9246707
Mario Lubenka <mario.lubenka@googlemail.com>
2020-11-22 20:30:33 +0100

    chore: restructure schema directory

--- validation/test.sh
+++ validation/test.sh
@@ -2,26 +2,33 @@
 
 TEST_FAILED=0
 
-# Valid files should be valid
-printf "Valid files should be valid:\n"
-for filename in examples/valid/*; do
-  if bin/project-validator "${filename}"; then
-  : # file is valid
-  else
-    TEST_FAILED=1
-    echo "File ${filename} is invalid." > /dev/stderr
-  fi
-done
+validate_files()
+{
+  SCHEMAFILE="schema/${1}.schema.json"
+  EXAMPLE_DIR="examples/${1}"
+  echo "### TESTING ${SCHEMAFILE} ... START\n"
+	printf "Valid %s files should be valid:\n" EXAMPLE_DIR
+	for filename in "${EXAMPLE_DIR}"/valid/*; do
+		if bin/jsonschema-validator "${SCHEMAFILE}" "${filename}"; then
+		: # file is valid
+		else
+			TEST_FAILED=1
+			echo "File ${filename} is invalid." > /dev/stderr
+		fi
+	done
+	printf "\n\n"
+	# Invalid files should be invalid
+	printf "Invalid %s files should be invalid:\n" EXAMPLE_DIR
+	for filename in "${EXAMPLE_DIR}"/invalid/*; do
+		if bin/jsonschema-validator "${SCHEMAFILE}" "${filename}"; then
+			TEST_FAILED=1
+			echo "File ${filename} is valid." > /dev/stderr
+		fi
+	done
+  echo "### TESTING ${SCHEMAFILE} ... END\n"
+}
 
-printf "\n\n"
-# Invalid files should be invalid
-printf "Invalid files should be invalid:\n"
-for filename in examples/invalid/*; do
-  if bin/project-validator "${filename}"; then
-    TEST_FAILED=1
-    echo "File ${filename} is valid." > /dev/stderr
-  fi
-done
+validate_files "project-definition"
 
 if [ $TEST_FAILED -eq 0 ]; then
   echo 'All tests succeeded.'
