73345e4fc78c73cc2a0679c3f927ab6022f0e1cd
Mario Lubenka <mario.lubenka@googlemail.com>
2020-11-23 19:06:28 +0100

    feat(action): add plugins option

--- ansible/__tests__/steps/setup.sh
+++ ansible/__tests__/steps/setup.sh
@@ -5,7 +5,7 @@ if [[ $INPUT_CLI != '' ]]; then
 	${INPUT_CLI_BIN_PATH} setup "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
 else
 	INVENTORY_PATH=${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.yml
-	sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${container}/${INPUT_CONTAINER}/" "${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.dist.yml" >"${INVENTORY_PATH}"
+	sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${container}/${INPUT_CONTAINER}/" -e "s/\${plugins}/${INPUT_PLUGINS}/" "${GITHUB_ACTION_PATH}/ansible/__tests__/inventory.dist.yml" >"${INVENTORY_PATH}"
 
 	# Provision server
 	TEST=1 ansible-playbook "${GITHUB_ACTION_PATH}/ansible/playbooks/server-provision.yml" -i "${INVENTORY_PATH}" -vv
