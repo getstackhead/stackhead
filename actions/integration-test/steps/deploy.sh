ee6deb5cebc5fb435088c9f6be9ecbffa4518e99
Mario Lubenka <mario.lubenka@googlemail.com>
2020-11-29 23:02:47 +0100

    test(cli): adjust CLI calls

--- actions/integration-test/steps/deploy.sh
+++ actions/integration-test/steps/deploy.sh
@@ -6,7 +6,7 @@ mkdir -p "${GITHUB_ACTION_PATH}/projects"
 sed -e "s/\${domain}/${INPUT_DOMAIN}/" -e "s/\${domain2}/${INPUT_DOMAIN2}/" "${GITHUB_ACTION_PATH}/project-definition.dist.yml" >"${GITHUB_ACTION_PATH}/projects/container.yml"
 
 if [[ $INPUT_CLI != '' ]]; then
-	${INPUT_CLI_BIN_PATH} deploy "${GITHUB_ACTION_PATH}/projects/container.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
+	${INPUT_CLI_BIN_PATH} project deploy "${GITHUB_ACTION_PATH}/projects/container.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
 else
 	INVENTORY_PATH="${GITHUB_ACTION_PATH}/inventory.yml"
 	sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${container}/${INPUT_CONTAINER}/" -e "s/\${application}/container/" -e "s/\${plugins}/${INPUT_PLUGINS}/" "${GITHUB_ACTION_PATH}/inventory.dist.yml" >"${INVENTORY_PATH}"
