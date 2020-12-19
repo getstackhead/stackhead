2b165c3bae9b889aaf5276ca60aa34a01786eaee
Mario Lubenka <mario.lubenka@googlemail.com>
2020-12-19 16:03:01 +0100

    chore(action): generate project definition in action folder

--- actions/integration-test/steps/deploy.sh
+++ actions/integration-test/steps/deploy.sh
@@ -3,10 +3,10 @@
 # IMPORTANT: This must run after test_provision.sh!
 
 mkdir -p "${GITHUB_ACTION_PATH}/projects"
-sed -e "s/\${domain}/${INPUT_DOMAIN}/" -e "s/\${domain2}/${INPUT_DOMAIN2}/" "${GITHUB_ACTION_PATH}/project-definition.dist.yml" >"${GITHUB_ACTION_PATH}/projects/container.yml"
+sed -e "s/\${domain}/${INPUT_DOMAIN}/" -e "s/\${domain2}/${INPUT_DOMAIN2}/" "${GITHUB_ACTION_PATH}/project-definition.dist.yml" >"${GITHUB_ACTION_PATH}/container.yml"
 
 if [[ $INPUT_CLI != '' ]]; then
-	${INPUT_CLI_BIN_PATH} project deploy "${GITHUB_ACTION_PATH}/projects/container.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
+	${INPUT_CLI_BIN_PATH} project deploy "${GITHUB_ACTION_PATH}/container.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
 else
 	INVENTORY_PATH="${GITHUB_ACTION_PATH}/inventory.yml"
 	sed -e "s/\${ipaddress}/${INPUT_IPADDRESS}/" -e "s/\${webserver}/${INPUT_WEBSERVER}/" -e "s/\${container}/${INPUT_CONTAINER}/" -e "s/\${application}/container/" -e "s/\${plugins}/${INPUT_PLUGINS}/" "${GITHUB_ACTION_PATH}/inventory.dist.yml" >"${INVENTORY_PATH}"
