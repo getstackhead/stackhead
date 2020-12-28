0aa0c70eeb0a93e6515934bf80e6d3dbb816c405
Mario Lubenka <mario.lubenka@googlemail.com>
2020-12-28 21:34:21 +0100

    chore: reset VERSION files

--- .github/workflows/release/prepare-files.sh
+++ .github/workflows/release/prepare-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # version file
-echo "v$1" > VERSION
-echo "v$1" > ansible/VERSION
+echo $1 > VERSION
+echo $1 > ansible/VERSION
 
 # update schemas in collection
 (cd schemas && find . -name "*.json" -exec cp --parents -R '{}' "../ansible/schemas/" ';')
