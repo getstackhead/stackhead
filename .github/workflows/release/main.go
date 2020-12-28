1938e35d3f336fcfe2fb35232c2c0c551646cc3f
Mario Lubenka <mario.lubenka@googlemail.com>
2020-12-28 17:00:53 +0100

    ci: adjust release hook

--- .github/workflows/release/main.go
+++ .github/workflows/release/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -24,7 +25,19 @@ func (t *HooksLogger) Version() string {
 	return "dev"
 }
 
+func (t *HooksLogger) writeVersionFile(version string, filePath string) error {
+	return ioutil.WriteFile(filePath, []byte(version), 0644)
+}
+
 func (t *HooksLogger) Success(config *hooks.SuccessHookConfig) error {
+	// adjust and copy version file ansible/VERSION /VERSION
+	// copy schemas from schemas/**.json to ansible/schemas/
+
+	// Write VERSION file
+	if err := t.writeVersionFile(config.NewRelease.Version, "VERSION"); err != nil {
+		return err
+	}
+
 	t.logger.Println("old version: " + config.PrevRelease.Version)
 	t.logger.Println("new version: " + config.NewRelease.Version)
 	t.logger.Printf("commit count: %d\n", len(config.Commits))
@@ -38,6 +51,23 @@ func (t *HooksLogger) NoRelease(config *hooks.NoReleaseConfig) error {
 }
 
 func main() {
+	// var logger = &HooksLogger{
+	// 	logger: log.New(os.Stderr, "", 0),
+	// }
+	// logger.Success(&hooks.SuccessHookConfig{
+	// 	PrevRelease: &semrel.Release{
+	// 		SHA:         "abc",
+	// 		Version:     "1.0.0",
+	// 		Annotations: nil,
+	// 	},
+	// 	NewRelease:  &semrel.Release{
+	// 		SHA:         "def",
+	// 		Version:     "2.0.0",
+	// 		Annotations: nil,
+	// 	},
+	// })
+	// panic("test")
+
 	plugin.Serve(&plugin.ServeOpts{
 		Hooks: func() hooks.Hooks {
 			return &HooksLogger{
