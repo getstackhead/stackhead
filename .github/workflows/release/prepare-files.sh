11010bdff9b1e898f032847b8ee62e9f5ef05d0a
Mario Lubenka <mario.lubenka@googlemail.com>
2020-12-29 19:16:24 +0100

    refactor: move schemas to Ansible collection
    Schema files for module-config and project-definition
    are moved from ./schemas/ to ./ansible/schemas/

--- .github/workflows/release/prepare-files.sh
+++ .github/workflows/release/prepare-files.sh
@@ -3,6 +3,3 @@
 # version file
 echo $1 > VERSION
 echo $1 > ansible/VERSION
-
-# update schemas in collection
-(cd schemas && find . -name "*.json" -exec cp --parents -R '{}' "../ansible/schemas/" ';')
