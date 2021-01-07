e7675479e993218b513eea6757fdf32ffd2db751
Mario Lubenka <mario.lubenka@googlemail.com>
2021-01-07 23:24:00 +0100

    chore(test): use staging server for integration test

--- actions/integration-test/steps/destroy.sh
+++ actions/integration-test/steps/destroy.sh
@@ -5,8 +5,8 @@
 # IMPORTANT: This must run after test_deploy.sh!
 
 if [[ $INPUT_CLI != '' ]]; then
-	${INPUT_CLI_BIN_PATH} project destroy "${GITHUB_ACTION_PATH}/container.stackhead.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
+	CI=1 ${INPUT_CLI_BIN_PATH} project destroy "${GITHUB_ACTION_PATH}/container.stackhead.yml" "${INPUT_IPADDRESS}" -c "/tmp/.stackhead-cli.yml" -v
 else
 	INVENTORY_PATH="${GITHUB_ACTION_PATH}/inventory.yml"
-	TEST=1 ansible-playbook "${STACKHEAD_CLONE_LOCATION}/ansible/playbooks/application-destroy.yml" -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
+	CI=1 ansible-playbook "${STACKHEAD_CLONE_LOCATION}/ansible/playbooks/application-destroy.yml" -i "${INVENTORY_PATH}" --extra-vars "project_name=container" -vv
 fi
