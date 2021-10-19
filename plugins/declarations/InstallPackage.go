0dcb5c032febd99307d2f9b51c1ee181503952e3
Mario Lubenka <mario.lubenka@googlemail.com>
2021-10-19 23:03:50 +0200

    feat: install packages logic

--- plugins/declarations/InstallPackage.go
+++ plugins/declarations/InstallPackage.go
@@ -1,7 +1,21 @@
 package declarations
 
-import "github.com/getstackhead/stackhead/pluginlib"
+import (
+	"github.com/getstackhead/stackhead/pluginlib"
+	"github.com/getstackhead/stackhead/stackhead"
+)
 
-var InstallPackage = func(packages []pluginlib.Package) {
-	// todo: how to deal with method input objects?! e.g. project...
+var InstallPackage = func(packages []pluginlib.Package) error {
+	for _, p := range packages {
+		if p.Vendor == pluginlib.PackageVendorApt {
+			if err := stackhead.RemoteRun("apt install " + p.Name); err != nil {
+				return err
+			}
+		} else if p.Vendor == pluginlib.PackageVendorApk {
+			if err := stackhead.RemoteRun("apk add " + p.Name); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
