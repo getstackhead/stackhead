f90c2e51840adbf0d8de17e366dad84860cc07b0
Mario Lubenka <mario.lubenka@googlemail.com>
2023-01-04 20:23:36 +0100

    chore: migrate Nginx module

--- modules/proxy/nginx/nginx.go
+++ modules/proxy/nginx/nginx.go
@@ -2,7 +2,6 @@ package proxy_nginx
 
 import (
 	"embed"
-	"text/template"
 
 	"github.com/getstackhead/stackhead/system"
 )
@@ -18,26 +17,8 @@ func (Module) GetTemplates() *embed.FS {
 }
 
 func (Module) GetConfig() system.ModuleConfig {
-	SnakeoilFullchainPath, SnakeoilPrivkeyPath := GetSnakeoilPaths()
 	return system.ModuleConfig{
 		Name: "nginx",
 		Type: "proxy",
-		Terraform: system.ModuleTerraformConfig{
-			Provider: system.ModuleTerraformConfigProvider{
-				Vendor:       "getstackhead",
-				Name:         "nginx",
-				Version:      "1.3.2",
-				ResourceName: "nginx_server_block",
-				Init:         "providers.tf.tmpl",
-				InitFuncMap: template.FuncMap{
-					"SnakeoilFullchainPath": func() string {
-						return SnakeoilFullchainPath
-					},
-					"SnakeoilPrivkeyPath": func() string {
-						return SnakeoilPrivkeyPath
-					},
-				},
-			},
-		},
 	}
 }
